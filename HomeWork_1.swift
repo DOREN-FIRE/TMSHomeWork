e6145a2eb6b9ed069cc45cccb8a0ffbf6d44165f
Danila <Danilazadorozhny@gmail.com>
2023-12-06 20:18:34 +0300

    HomeWork_3
    - Вычислить сумму цифр четырехзначного числа
    - Функция сравнения строк - "авб" больше "ввш"
    - Циклический вызов функций - поломать приложение
    - Функция возведения в степень с дефолтным параметром

---
+++ HomeWork_1.swift
@@ -0,0 +1,8 @@
+//
+//  HomeWork_1.swift
+//  Lessons 1, 2
+//
+//  Created by Данила Витальевич on 6.12.23.
+//
+
+import Foundation
--- Lessons 1, 2/ViewController.swift
+++ Lessons 1, 2/ViewController.swift
@@ -29,8 +29,14 @@ var secondInt = Int (second)
 var thirdInt = Int(third)
 var forthInt = Int (forth)
 
-var sumOfIntNumbers = firstInt + secondInt + thirdInt + forthInt
-print ("Сумма целых частей чисел равна" + String(sumOfIntNumbers))
+func CalcSumOfIntNumbers(numbers: Array<Int>) -> Int {
+    var sum = 0
+    for number in numbers {
+        sum += number
+    }
+    return sum
+}
+var sumOfIntNumbers = CalcSumOfIntNumbers(numbers: [firstInt, secondInt, thirdInt, forthInt])
 
 // сумма отдельно дробных частей чисел
 var firstFraction = first - Double(firstInt)
@@ -38,21 +44,28 @@ var secondFraction = second - Double (secondInt)
 var thirdFraction = third - Double(thirdInt)
 var forthFraction = forth - Double(forthInt)
 
-var sumOfFractions = firstFraction + secondFraction + thirdFraction + forthFraction
-print ("Сумма дробных частей чисел равна " + String (sumOfFractions))
-
-//проверка является ли число четным для целых чисел
-if sumOfIntNumbers % 2 == 0 {
-print (String(sumOfIntNumbers) + " - это четное число")
-} else {
-    print (String (sumOfIntNumbers) + " - это нечетное число")
-
-    // проверка является ли число четным для дробных чисел
-    var checkingFractions = (sumOfFractions).truncatingRemainder(dividingBy: (2.0)) == 0
-    if checkingFractions == true {
-        print (String(Float(sumOfFractions)) + "- это четное число")
-    } else {
-        print (String(Float(sumOfFractions)) + " - это нечетное число")
+func CalcSumOfFractionalNumbers(numbers: Array<Double>) -> Double {
+    var sum = 0.0
+    for number in numbers {
+        sum += number
     }
-    
+    return sum
 }
+var sumOfFractionalNumbers = CalcSumOfFractionalNumbers(numbers: [firstFraction, secondFraction, thirdFraction, forthFraction])
+
+////проверка является ли число четным для целых чисел
+//if sumOfIntNumbers % 2 == 0 {
+//print (String(sumOfIntNumbers) + " - это четное число")
+//} else {
+//    print (String (sumOfIntNumbers) + " - это нечетное число")
+//
+//    // проверка является ли число четным для дробных чисел
+//    var checkingFractions = (sumOfFractions).truncatingRemainder(dividingBy: (2.0)) == 0
+//    if checkingFractions == true {
+//        print (String(Float(sumOfFractions)) + "- это четное число")
+//    } else {
+//        print (String(Float(sumOfFractions)) + " - это нечетное число")
+//    }
+//    
+//}
+
